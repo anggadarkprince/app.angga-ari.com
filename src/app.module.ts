2fa1531cb97ce3b101f385c5da967f2931a6db9d
Angga Ari Wijaya <anggadarkprince@gmail.com>
2022-04-08 09:03:18 +0700

    add some security setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 import {TerminusModule} from "@nestjs/terminus";
 import {HttpModule} from "@nestjs/axios";
+import {ThrottlerModule} from "@nestjs/throttler";
 
 @Module({
   imports: [
       HttpModule,
       TerminusModule,
+      ThrottlerModule.forRoot({
+          ttl: 60,
+          limit: 10,
+      }),
   ],
   controllers: [AppController, HealthController],
   providers: [AppService],
